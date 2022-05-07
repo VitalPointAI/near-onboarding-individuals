54a242254b25d7638d9cd780efb0edbf58c73c1d
Aaron Luhning <a.luhning@vitalpoint.ai>
2022-05-07 12:55:14 -0400

    tst

--- api/token/index.js
+++ api/token/index.js
@@ -1,4 +1,27 @@
 
+// const { DefaultAzureCredential } = require("@azure/identity")
+// const { SecretClient } = require("@azure/keyvault-secrets")
+
+// const credential = new DefaultAzureCredential()
+// console.log('credential', credential)
+
+const vaultName = process.env.VAULT_NAME
+console.log('vault name', vaultName)
+
+const url = `https://${vaultName}.vault.azure.net`
+console.log('url', url)
+
+const client = new SecretClient(url, credential)
+console.log('client', client)
+
+const secretKey = process.env.SECRET_KEY
+console.log('secretkey', secretKey)
+// const secretSeed = process.env.SEED
+// const fundingSeed = process.env.FUNDING_SEED
+// const sendyAPI = process.env.SENDY_API
+
+// const latestTokenSecret = await client.getSecret(secretKey)
+// console.log('latesttoken', latestTokenSecret)
 
 module.exports = async function (context, req) {
     // const accountId = req.body.accountId
