041522dd0df15986f35b48b7260c99a20ac14009
Aaron Luhning <a.luhning@vitalpoint.ai>
2022-05-07 22:35:05 -0400

    yep

--- api/seed/index.js
+++ api/seed/index.js
@@ -10,13 +10,31 @@ const url = `https://${vaultName}.vault.azure.net`
 
 const client = new SecretClient(url, credential)
 
-//const secretKey = process.env.SECRET_KEY
+const secretKey = process.env.SECRET_KEY
 const secretSeed = process.env.SEED
 //const fundingSeed = process.env.FUNDING_SEED
 //const sendyAPI = process.env.SENDY_API
 
-module.exports = async function (context, req) {
+// Verify Token
+function verifyToken(req, res, next){
+    // Get auth header value
+    const bearerHeader = req.headers['authorization'];
+    // Check if bearer is undefined
+    if(typeof bearerHeader !== 'undefined'){
+      // Split at the space
+      const bearerToken = bearerHeader.split(' ')[1];
+      // Set the token
+      req.token = bearerToken;
+      // Next middleware
+      next();
+    } else {
+      //Forbidden
+      res.sendStatus(403);
+    }
+}
 
+module.exports = async function (context, req) {
+  const latestTokenResponse = await client.getSecret(secretKey)
   jwt.verify(req.token, latestTokenResponse.value, async (err, authData) => {
     if(err) {
       context.res.sendStatus(403);
