59a604b866222e1db0ec465b4d99896842744723
Aaron Luhning <a.luhning@vitalpoint.ai>
2022-06-18 14:16:46 -0400

    remove 0 from staking

--- api/token-mainnet/index.js
+++ api/token-mainnet/index.js
@@ -5,12 +5,6 @@ module.exports = async function (context, req) {
     const accountId = req.body.accountId
     if(!accountId) context.res.sendStatus(403)
     let token = jwt.sign({accountId: accountId }, process.env["PERSONAS_MAINNET_SECRET_KEY"])
-    // jwt.sign({ accountId: accountId }, process.env["PERSONAS_MAINNET_SECRET_KEY"], (err, token) => {
-    //   context.res.json({
-    //     token
-    //   })
-    // });
-    context.log('token', token)
     if(token){
       context.res.json({
         token
