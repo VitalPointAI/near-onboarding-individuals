d30627f64ad0753728617d360aa29e30ec8f1c1b
Aaron Luhning <a.luhning@vitalpoint.ai>
2022-06-26 14:27:16 -0400

    fix

--- src/components/common/AccountTransactionActivity/accountTransactionActivity.js
+++ src/components/common/AccountTransactionActivity/accountTransactionActivity.js
@@ -368,7 +368,7 @@ export default function AccountTransactionActivity(props) {
         // })
         let amount = (parseFloat(cleanValue) * price).toFixed(2)
         console.log('amount', amount)
-        if(label != 'stake' && amount != '0' && date >= new Date(fromDate) && date <= new Date(toDate)){
+        if(label != 'stake' && amount != '0.00' && date >= new Date(fromDate) && date <= new Date(toDate)){
           csvToQuicken.push({
             Date: date,
             Amount: amount,
